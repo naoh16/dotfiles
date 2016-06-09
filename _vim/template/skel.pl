6debade340840e7a23803bed01cfe041c43c427f
Sunao Hara <hara@csnx2.gss.cs.okayama-u.ac.jp>
2016-06-09 11:41:58 +0900

    Update templates   - add EXIT trap

--- _vim/template/skel.pl
+++ _vim/template/skel.pl
@@ -2,30 +2,20 @@
 #
 #
 #
-#   Copyright (C) 2014 Sunao HARA (hara@okayama-u.ac.jp)
-#   Copyright (C) 2014 Abe Laboratory, Okayama Univresity
-#   Last Modified: 2014/07/11 11:38:18.
+#   Copyright (C) 2016 Sunao HARA (hara@okayama-u.ac.jp)
+#   Copyright (C) 2016 Abe Laboratory, Okayama Univresity
+#   Last Modified: 2016/05/31 18:50:57.
 #
-##
-## 定番のおまじない
-##
 use strict;
-use Data::Dumper; { package Data::Dumper; sub qquote { return shift; } } $Data::Dumper::Useperl = 1;
+#use Data::Dumper; { package Data::Dumper; sub qquote { return shift; } } $Data::Dumper::Useperl = 1;
 use warnings;
 use 5.10.0;
-
-# Usage of Data::Dumper; print Dumper(\@data);
-#use Text::CSV::Simple;   # $p = Text::CSV::Simple->new({binary=>1}); @d = $p->read_file($filename);
-#use Text::CSV_XS;        # $csv = Text::CSV_XS->new({binary=>1}); while($row = $csv->getline(IN));
-#use Encode;              # Memo: euc->sjis; Encode::from_to($hoge, "ujis", "sjis");
 ## Encodings
 #use utf8;
 #use encoding "euc-jp";
 #use open IO => ":encoding(euc-jp)";
 
-##
 ## 強制終了 トラップ
-##
 $SIG{'INT'}=$SIG{'HUP'}=$SIG{'QUIT'}=$SIG{'TERM'}=sub { print "SIGINT!!\n"; exit; };
 
 ##
--- _vim/template/skel.sh
+++ _vim/template/skel.sh
@@ -2,11 +2,11 @@
 #
 #
 #
-#    Copyright (C) 2014 Sunao HARA (hara@okayama-u.ac.jp)
-#    Copyright (C) 2014 Abe laboratory, Okayama university
-#    Last Modified: 2014/07/11 11:22:06.
+#    Copyright (C) 2016 Sunao HARA (hara@okayama-u.ac.jp)
+#    Copyright (C) 2016 Abe laboratory, Okayama university
+#    Last Modified: 2016/06/09 11:39:29.
 #
 
 CWD=${0%/*}
-
+trap '[ -n "$(jobs -p)" ] && kill $(jobs -p)' EXIT
 
